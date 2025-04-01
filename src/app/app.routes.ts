48fa1aee36b7007fbf1c6018884c6cd2d957d0a5
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2025-04-01 16:37:41 +0200

    Creacion del Proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { QuoteComponent } from './functions/quote/component/quote/quote.component';
+import { DownloadComponent } from './functions/download/components/download/download.component';
+import { AssetComponent } from './functions/asset/components/asset/asset.component';
+import { HomeComponent } from './functions/dashboard/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'assets', component: AssetComponent },
+    { path: 'download', component: DownloadComponent },
+    { path: 'quotes', component: QuoteComponent },
+  ];
